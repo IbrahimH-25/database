4a9ebc3fd037addf40c79af2bb9db9b4e1a549c1
Ibrahim-gw8885 <gw8885@wayne.edu>
2023-11-16 22:21:51 -0500

    passwords as john1234

--- src/userDAO.java
+++ src/userDAO.java
@@ -45,7 +45,7 @@ public class userDAO
             } catch (ClassNotFoundException e) {
                 throw new SQLException(e);
             }
-            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/DavesTimber?allowPublicKeyRetrieval=true&useSSL=false&user=john&password=pass1234");
+            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/DavesTimber?allowPublicKeyRetrieval=true&useSSL=false&user=john&password=john1234");
             System.out.println(connect);
         }
     }
