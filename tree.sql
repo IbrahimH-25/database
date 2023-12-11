2e42749797cf010a72bb70256223f5dfdb4d56ff
Aladdin47 <148783989+Aladdin47@users.noreply.github.com>
2023-12-11 09:10:55 -0500

    Update tree.sql

--- tree.sql
+++ tree.sql
@@ -8,7 +8,10 @@ CREATE TABLE if not exists Tree(
     size VARCHAR(10) NOT NULL, 
     height VARCHAR(10) NOT NULL, 
 	location VARCHAR(100) NOT NULL, 
+	photo1 longblob NOT NULL,
+	photo2 longblob NOT NULL,
+	photo3 longblob NOT NULL,
     PRIMARY KEY (treeID) ); 
-    insert into Tree(treeID, size, height, location)
+    insert into Tree(treeID, size, height, location, photo1, photo2, photo3)
     values ('000001', '8 feet', '25 feet','yes');
 select * from Tree;            
